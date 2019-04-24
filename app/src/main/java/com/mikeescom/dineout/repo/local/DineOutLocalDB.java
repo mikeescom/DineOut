83441a20469ea60b04daf133d43d85d6c3919c8d
Miguel Saavedra Lozano <miguel.l@partner.samsung.com>
2019-04-24 14:25:32 -0700

    Change launch icon, adding splash screen image and fixing DB problem with Collections object.

--- app/src/main/java/com/mikeescom/dineout/repo/local/DineOutLocalDB.java
+++ app/src/main/java/com/mikeescom/dineout/repo/local/DineOutLocalDB.java
@@ -6,7 +6,7 @@ import android.arch.persistence.room.RoomDatabase;
 import com.mikeescom.dineout.repo.local.dbobjects.DBCategory;
 import com.mikeescom.dineout.repo.local.dbobjects.DBCity;
 
-@Database(entities = {DBCategory.class, DBCity.class}, version = 2, exportSchema = false)
+@Database(entities = {DBCategory.class, DBCity.class, DBCollection.class}, version = 2, exportSchema = false)
 public abstract class DineOutLocalDB extends RoomDatabase {
     public abstract CategoriesDao categoriesDao();
     public abstract CitiesDao citiesDao();
